416934fcf432d28e5a4fbfdcab694a4defc3e37f
Nagy Zétény <nagyzeteny6@gmail.com>
2023-06-01 10:46:59 +0200

    sort paths in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from 'next';
 import { items } from '~/constants/items';
 
 const sitemap = (): MetadataRoute.Sitemap => {
-  return ['', ...items.map((item) => item.path)].map((route) => ({
+  return ['', ...items.map((item) => item.path)].sort().map((route) => ({
     url: `https://random.znagy.hu${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
